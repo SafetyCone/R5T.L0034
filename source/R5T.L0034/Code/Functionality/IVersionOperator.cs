747a25533c387a5cdd5187ff54ea8a2c5b59f797
davidcoats <david@getsafetycone.com>
2023-10-18 10:46:11 -0700

    Interim changes.

--- source/R5T.L0034/Code/Functionality/IVersionOperator.cs
+++ source/R5T.L0034/Code/Functionality/IVersionOperator.cs
@@ -7,7 +7,8 @@ namespace R5T.L0034
 {
     [FunctionalityMarker]
     public partial interface IVersionOperator : IFunctionalityMarker,
-        F0124.IVersionOperator
+        F0124.IVersionOperator,
+        L0053.IVersionOperator
     {
 
     }
